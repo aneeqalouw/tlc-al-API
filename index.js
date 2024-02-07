90b647731406b51d3f31289a26a7f31614b9f1be
tlcgeyer <tamlincupido2402@gmail.com>
2024-02-07 14:01:55 +0200

    server

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const port = 
+    http.createServer((req, res) => {
+    res.writeHead(200)
+    res.end("WELCOME to our exercise. Low-key dying here ToT.")
+});
+
+//Here we make the server listen to the port 6969
+port.listen(6969, () => {
+    console.log(`Server running on : http://localhost:${6969}`);
+});
+
